73ff28747ff2e14d391ca8ede8361f4254379ae0
yoshikyoto <admin@h96.26.238.10.30790.vlan.kuins.net>
2014-06-16 10:04:16 +0900

    commti for repair master

--- src/SlidePDF.java
+++ src/SlidePDF.java
@@ -1,14 +1,19 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.List;
 
 import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDResources;
 import org.apache.pdfbox.util.PDFTextStripper;
 
 
 class SlidePDF{
 	String pdfPath;
 	File pdfFile;
+	int page;
+	List<PDPage> pageList;
 	
 	SlidePDF(String path){
 		pdfPath = path;
@@ -26,6 +31,16 @@ class SlidePDF{
 			String text = pdf_text_stripper.getText(document);
 			
 			System.out.println(text);
+			
+			page = document.getNumberOfPages();
+			pageList = document.getDocumentCatalog().getAllPages();
+			
+			for(int i = 0; i < page; i++){
+				PDPage target_page = pageList.get(i);
+				PDResources resources = target_page.getResources();
+				
+			}
+				
 		}catch(Exception e){
 			e.printStackTrace();
 		}
