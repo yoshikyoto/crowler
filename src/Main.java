83095e132c57cfbc6ef279b0d39d93203831877a
yoshikyoto <admin@h96.26.238.10.30790.vlan.kuins.net>
2014-07-03 01:30:08 +0900

    Cosim実装

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.util.ArrayDeque;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Queue;
 
@@ -16,11 +18,32 @@ class Main{
 			"ocw.u-tokyo.ac.jp",
 			"ocw.nagoya-u.jp"
 		};
+
+		Map<String, Double> ma = new HashMap<String, Double>();
+		Map<String, Double> mb = new HashMap<String, Double>();
+		Map<String, Double> mc = new HashMap<String, Double>();
 		
+		ma.put("hoge", 1.0);
+		ma.put("huga", 2.5);
+		ma.put("foo", 1.0);
+		
+		mb.put("hoge", 2.0);
+		mb.put("huga", 5.0);
+		mb.put("foo", 2.0);
+		
+		mc.put("foo", 1.2);
+		mc.put("bar", 2.3);
+
+		System.out.println(Cosim.calc(ma, mb));
+		System.out.println(Cosim.calc(ma, mc));
+		
+		
+		/*
 		KyotoOCW k = new KyotoOCW();
 		k.getCourseLists();
 		k.getLectures();
 		k.getPDFs();
+		*/
 		
 		/*
 		for(String domain : domains){
