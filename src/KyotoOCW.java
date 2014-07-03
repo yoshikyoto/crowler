1ad9559f94e1ec54acf8d82e7523a7e825b45876
yoshikyoto <admin@h96.26.238.10.30790.vlan.kuins.net>
2014-07-03 10:17:01 +0900

    add superclass OCW

--- src/KyotoOCW.java
+++ src/KyotoOCW.java
@@ -14,14 +14,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-class KyotoOCW{
-	String domain = "ocw.kyoto-u.ac.jp";
-	public Queue<String> urlQueue;
-	public Queue<String> lecturenoteQueue;
-	public Queue<String> pdfQueue;
-	public Queue<Lecture> lectureQueue;
+class KyotoOCW extends OCW{
 	private static final boolean DEBUG = false;
-	public int lecturenum = 0;
+	
+	KyotoOCW(){
+		 domain = "ocw.kyoto-u.ac.jp";
+	}
 	
 	public void getCourseLists(){
 		// ディレクトリを作成して
@@ -35,11 +33,6 @@ class KyotoOCW{
 	}
 	
 	public void getCourseList(String courselist_url_str){
-		// String courselist_url_str = "http://ocw.kyoto-u.ac.jp/courselist";
-		// ディレクトリは作っておく
-		// String http_result[] = get(courselist_url);
-		// retrieveURLs(http_result[0], courselist_url);
-		
 		try{
 			// GET
 			String line = new String();
@@ -105,7 +98,7 @@ class KyotoOCW{
 			
 			System.out.println("講義名: " + lecture_name);
 			
-			// 講義のノートのページのURL取得 FIXME: 必ずしもlecturenoteという名前じゃない
+			// 講義のノートのページのURL取得 FIXME: 必ずしもlecturenoteという名前じゃないようだ
 			http_results = get(url_str + "/lecturenote");
 			if(http_results == null) continue;
 			
@@ -121,40 +114,7 @@ class KyotoOCW{
 		}
 	}
 	
-	ArrayList<String> retrievePDF(String str){
-		Pattern pattern = Pattern.compile("<a .*?href *?= *?\"(.+?\\.pdf)\".*?>");
-		Matcher matcher = pattern.matcher(str);
-		ArrayList<String> result = new ArrayList<String>();
-		while(matcher.find()){
-			String url_str = matcher.group(1);
-			if(DEBUG) System.out.println("Find URL: " + url_str);
-			
-			// remove after #
-			int sharp_index = url_str.indexOf("#");
-			if(sharp_index >= 0)
-				url_str = url_str.substring(0, sharp_index);
-			
-			if(url_str.length() == 0) continue;
-			
-			// httpから始まる場合
-			if(url_str.indexOf("http://") == 0){
-				// 同じドメインかどうか
-				if(url_str.indexOf("http://" + domain) != 0) continue;
-				// full URL in kyoto-u domain
-				if(DEBUG) System.out.println("Find Full Path URL");
-			// httpから始まらない場合
-			}else if(url_str.charAt(0) == '/'){
-				// 絶対パスの場合
-				url_str = "http://" + domain + url_str;
-			}else{
-				url_str = "";
-			}
-			
-			if(url_str.length() != 0 && !result.contains(url_str))
-				result.add(url_str);
-		}
-		return result;
-	}
+
 	
 	public void getPDFs(){
 		while(lectureQueue.size() > 0){
@@ -163,41 +123,7 @@ class KyotoOCW{
 		}
 	}
 	
-	private String[] get(String url_str){
-		try{
-			String response_strs[] = new String[2];
-			URL url = new URL(url_str);
-			System.out.println("GET " + url);
-			
-			if(DEBUG){
-				System.out.println("URL getFile: " + url.getFile());
-				System.out.println("URL getPath: " + url.getPath());
-			}
-			
-			HttpURLConnection http_connection = (HttpURLConnection)url.openConnection();
-			http_connection.connect();
-			InputStreamReader isr = new InputStreamReader(http_connection.getInputStream(), "UTF-8");
-			BufferedReader br = new BufferedReader(isr);
-			
-		    response_strs[1] = http_connection.getContentType();
-		    System.out.println("Content-Type: " + response_strs[1]);
-		    if(response_strs[1].indexOf("html") == -1){
-		    	response_strs[0] = "";
-		    	return response_strs;
-		    }
 
-			String line = new String();
-			while((line = br.readLine()) != null){
-				response_strs[0] += line + "\n";
-			}
-			if(DEBUG) System.out.println(response_strs[0]);
-			return response_strs;
-			
-		} catch (Exception e) {
-			System.out.println(e);
-			return null;
-		}
-	}
 	
 	private void retrieveURLs(String str, String current_url_str){
 		System.out.println("Retrieve URLs");
