46e16ae239800e78e97ecb4aa1400122d157b4eb
yoshikyoto <admin@pc-a320.local>
2014-11-14 15:38:20 +0900

    refactor XML Converter
    PdfXMLConverter を slide.XMLConverter パッケージに追加
    gitignore に DS_Store を追加

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 import jp.dip.utakatanet.*;
+import slide.XMLConverter.*;
 
 class Main extends Base{
 	
@@ -11,6 +12,7 @@ class Main extends Base{
 	 * ocw.u-tokyo.ac.jp
 	 * ocw.nagoya-u.jp
 	 */
+	public static String root = "/Users/admin/ocwslidedata";
 	public static void main(String args[]){
 		String domains[] = {
 			"ocw.kyoto-u.ac.jp",
@@ -20,10 +22,42 @@ class Main extends Base{
 		
 		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Input number");
 		switch(sc.nextInt()){
+		case 0: // Crawling?
+			break;
+		case 1: // slide/pdf to XML
+			slideToXML();
+			
+			break;
+		case 2: // slide/pdf to Image
+			break;
 		}
 		
-		SlideData d = new SlideData("/Users/admin/ocwslidedata");
+		// SlideData d = new SlideData("/Users/admin/ocwslidedata");
+	}
+	
+	public static void slideToXML(){
+		Logger.setLogName("ConvertXML");
+		
+		for(File ocwfile : listDirs(root)){
+			// 各 OCW のディレクトリについて
+			Logger.sPrintln("OCW: " + ocwfile.getName());
+			
+			for(File lecfile : listDirs(ocwfile.getAbsolutePath())){
+				// 各講義のディレクトリについて
+				Logger.sPrintln("Lecture: " + lecfile.getName());
+				
+				for(File sfile : listDirs(lecfile.getAbsolutePath())){
+					// 各スライドのファイルに対して
+					Logger.sPrintln("Slide: " + sfile.getName());
+					
+					SlideXMLConverter.convert(sfile.getAbsolutePath());
+				}
+			}
+		}
+		Logger.sClose();
+	}
 
 		/*
 		PPTFiles pf1 = new PPTFiles("/Users/admin/eclipse_workspace/Slide/testdata_kyoto-u");
@@ -136,5 +170,4 @@ class Main extends Base{
 		
 		// SlidePDF slide_pdf = new SlidePDF("ocw.kyoto-u.ac.jp/dip_01.pdf");
 		// slide_pdf.parse();
-	}
 }
--- src/jp/dip/utakatanet/Logger.java
+++ src/jp/dip/utakatanet/Logger.java
@@ -5,7 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-class Logger{
+public class Logger{
 	public static PrintWriter p, ep;
 	public PrintWriter pw, epw;
 
